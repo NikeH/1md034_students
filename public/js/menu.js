f20e2ee852c193cf7ebb5a7b8adcec8784647247
Nike Hiller <nike.hiller@openhack.io>
2020-02-22 19:38:46 +0100

    Task 4-9 completed

---
+++ public/js/menu.js
@@ -0,0 +1,47 @@
+let food = [{
+        "name": "No chicken",
+        "ingredients": "vegan",
+        "kCal": 750,
+        "lactose": false,
+        "gluten": true,
+        "img": "https://media.giphy.com/media/66e8QBYiL0COs/giphy.gif"
+    },
+
+    {
+        "name": "Plant beef",
+        "ingredients": "veggie",
+        "kCal": 650,
+        "lactose": false,
+        "gluten": true,
+        "img": "https://media.giphy.com/media/WBTUlyKBVlTTa/giphy.gif"
+    },
+
+    {
+        "name": "Halloumi",
+        "ingredients": "veggie",
+        "kCal": 670,
+        "lactose": true,
+        "gluten": false,
+        "img": "https://media.giphy.com/media/yoJC2JdQQSQA3PWovu/giphy.gif"
+    },
+
+    {
+        "name": "Tripple fake beef",
+        "ingredients": "veggie",
+        "kCal": 730,
+        "lactose": true,
+        "gluten": false,
+        "img": "https://media.giphy.com/media/3oEdv5S8Th6b9gsNqM/giphy.gif"
+    },
+
+    {
+        "name": "Quatro quesos",
+        "ingredients": "veggie",
+        "kCal": 720,
+        "lactose": true,
+        "gluten": false,
+        "img": "https://media.giphy.com/media/l4Ki5FgQxmVUfkOvC/giphy.gif"
+    },
+
+
+]
